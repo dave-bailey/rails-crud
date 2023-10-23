0b0cb07caa86516e246496501465f153bc6663f9
Dave Bailey <dave.andrew.bailey@gmail.com>
2023-10-23 12:07:07 -0400

    feat: route for post by id

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "posts#index"
-  get "/posts", to: "posts#index" 
+  get "/posts", to: "posts#index"
+  get "/posts/:id", to: "posts#show"
 end
