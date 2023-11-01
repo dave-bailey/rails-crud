e5f9526173dc055583dbb851580a53cc31dd238a
Dave Bailey <dave.andrew.bailey@gmail.com>
2023-11-01 10:22:30 -0400

    feat: add rich text editor

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   include Visible
   
   has_many :comments, dependent: :destroy
+  has_rich_text :body
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
